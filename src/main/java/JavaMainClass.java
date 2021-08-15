68a769e9456cf9f64b54a1193dde54e7901d9745
Sviatlana Kisel <kisels1.20@gmail.com>
2021-08-15 23:53:22 +0300

    add task 2

--- src/main/java/JavaMainClass.java
+++ src/main/java/JavaMainClass.java
@@ -1,7 +1,13 @@
 public class JavaMainClass {
 
-    public static int getLocalNumber(){
+    private static int class_number = 20;
+
+    public static int getLocalNumber() {
         int localNumber = 14;
         return localNumber;
+    }
+
+    public static int getClassNumber() {
+        return class_number;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -4,11 +4,19 @@ import org.junit.Test;
 public class MainClassTest {
 
     private int localNumber = 14;
+    private int classNumber = 45;
 
     @Test
     public void testGetLocalNumber() {
         int actualLocalNumber = JavaMainClass.getLocalNumber();
         Assert.assertEquals(String.format("Expected localNumber - %d, but actual is %d", localNumber, actualLocalNumber),
                 localNumber, actualLocalNumber);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        int actualClassNumber = JavaMainClass.getClassNumber();
+        Assert.assertTrue(String.format("Actual classNumber %d is not > %d", actualClassNumber, classNumber),
+                actualClassNumber > classNumber);
     }
 }
