90a005baf46f8db946c21e54ff181dde23ad765e
Sviatlana Kisel <kisels1.20@gmail.com>
2021-08-16 00:21:47 +0300

    add task 3

--- src/main/java/JavaMainClass.java
+++ src/main/java/JavaMainClass.java
@@ -1,6 +1,7 @@
 public class JavaMainClass {
 
     private static int class_number = 20;
+    private String class_string = "Hello, world";
 
     public static int getLocalNumber() {
         int localNumber = 14;
@@ -9,5 +10,9 @@ public class JavaMainClass {
 
     public static int getClassNumber() {
         return class_number;
+    }
+
+    public String getClassString(){
+        return class_string;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -9,8 +9,8 @@ public class MainClassTest {
     @Test
     public void testGetLocalNumber() {
         int actualLocalNumber = JavaMainClass.getLocalNumber();
-        Assert.assertEquals(String.format("Expected localNumber - %d, but actual is %d", localNumber, actualLocalNumber),
-                localNumber, actualLocalNumber);
+        Assert.assertEquals(String.format("Expected localNumber - %d, but actual is %d", localNumber,
+                actualLocalNumber), localNumber, actualLocalNumber);
     }
 
     @Test
@@ -18,5 +18,15 @@ public class MainClassTest {
         int actualClassNumber = JavaMainClass.getClassNumber();
         Assert.assertTrue(String.format("Actual classNumber %d is not > %d", actualClassNumber, classNumber),
                 actualClassNumber > classNumber);
+    }
+
+    @Test
+    public void testGetClassString() {
+        String classStringSubString1 = "hello";
+        String classStringSubString2 = "Hello";
+        String actualClassString = new JavaMainClass().getClassString();
+        Assert.assertTrue(String.format("Actual classString '%s' is not contain any of ['%s', '%s']", actualClassString,
+                classStringSubString1, classStringSubString2),
+                actualClassString.contains(classStringSubString1) || actualClassString.contains(classStringSubString2));
     }
 }
